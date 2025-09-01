02e6886f131282c157e989a529a64f6ded4a2ae5
Iván Felipe Benavides <pipeben@gmail.com>
2025-09-01 00:00:02 +0000

    version 0.0.5

--- R/get_imputations.R
+++ R/get_imputations.R
@@ -20,7 +20,7 @@
 #'    \item{"stl" - Seasonal and trend decomposition with Loess}
 #'    }
 #'
-#'    For further details on these imputation methods please check packages \code{\link{imputeTS}} and \code{\link{forecast}}
+#'    For further details on these imputation methods please check packages \CRANpkg{imputeTS} and \CRANpkg{forecast}
 #'
 #' @param seed Numeric. Any number
 #'
